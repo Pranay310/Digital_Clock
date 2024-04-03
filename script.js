fe2576d6e62d9a5097181c9a9c7a42d8a8eaa2e9
Pranay310 <146696037+Pranay310@users.noreply.github.com>
2024-04-03 16:43:26 +0530

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 function displayTime(){
 
-	let hrs = document.getElementById("hrs");
-	let mins = document.getElementById("mins");
-	let secs = document.getElementById("secs");
-
-	let dt = new Date();
-	hrs.innerHTML = (dt.getHours())%12;
-	mins.innerHTML = dt.getMinutes();
-	secs.innerHTML = dt.getSeconds();
 }
 
-setInterval(displayTime,1000)
+
